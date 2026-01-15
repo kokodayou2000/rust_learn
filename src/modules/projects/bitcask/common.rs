b12d0d61bbb07582a624715a3ce47589f7948793
koko <1611601667@qq.com>
2026-01-15 15:57:52 +0800

    feat: remove bitcask

--- src/modules/projects/bitcask/common.rs
+++
@@ -1,6 +0,0 @@
-pub const KEY_VAL_HEADER_LEN: u32 = 4;
-pub const MERGE_FILE_EXT: &str = "merger";
-
-pub type Result<T> = std::result::Result<T, std::io::Error>;
-
-pub type KeyDir = std::collections::BTreeMap<Vec<u8>, (u64, u32)>;
--- src/modules/projects/bitcask/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod log;
-pub mod index;
-mod common;
-mod scan_iterator;
