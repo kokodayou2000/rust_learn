b77ed8b2313ec608a3670a16a2bc33b4d38f7ddc
koko <1611601667@qq.com>
2026-02-03 18:11:52 +0800

    feat: remove arrow-rs-object-store

--- src/modules/projects/object_store/mod.rs
+++
@@ -1,9 +0,0 @@
-mod lib;
-mod tags;
-mod attributes;
-mod buffered;
-mod path;
-mod payload;
-mod aws;
-mod util;
-mod upload;
--- src/modules/projects/object_store/payload.rs
+++
@@ -1,6 +0,0 @@
-use std::sync::Arc;
-use bytes::Bytes;
-
-
-#[derive(Debug,Clone)]
-pub struct PutPayload(Arc<[Bytes]>);
